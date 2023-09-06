7b7a318c6afc940984e97d7a54e6d1b6e939cc03
unknown <t.ben-arfa@cliqeo.com>
2023-09-06 17:30:10 +0200

    update your copoments  congé

--- src/components/Conge/Conge.jsx
+++ src/components/Conge/Conge.jsx
@@ -2,27 +2,32 @@ import React, { useState, useEffect } from "react";
 
 import { useNavigate } from "react-router-dom";
 import toast, { Toaster } from "react-hot-toast";
-import Topbar from '../../layout/Topbar/Topbar';
+import Topbar from "../../layout/Topbar/Topbar";
+import { Button, Modal } from "react-bootstrap";
 import DatePicker from "react-datepicker";
-import fr from 'date-fns/locale/fr';
+import fr from "date-fns/locale/fr";
 
 import "react-datepicker/dist/react-datepicker.css";
-import './Conge.css'
+import "./Conge.css";
 
 import axios from "axios";
 import { useJwt } from "react-jwt";
 import Cookies from "universal-cookie";
 const baseURL = process.env.REACT_APP_API_URL;
 function Conge() {
+  const [AllConge, setAllConge] = useState([]);
+  const [conge, setConge] = useState('');
   const [nom, setNom] = useState("");
   const [jour, setJour] = useState(0);
-  const [description, setDescription] = useState('');
+  const [description, setDescription] = useState("");
   const [dateRange, setDateRange] = useState([null, null]);
+  const [show, setShow] = useState(false);
+  const handleClose = () => setShow(false);
   const [startDate, endDate] = dateRange;
   const createUser = async () => {
     try {
-      const formattedStartDate = startDate.toISOString().split('T')[0];
-      const formattedEndDate = endDate.toISOString().split('T')[0];
+      const formattedStartDate = startDate.toISOString().split("T")[0];
+      const formattedEndDate = endDate.toISOString().split("T")[0];
       await axios
         .post(baseURL + "/conges/create", {
           name: nom,
@@ -32,7 +37,6 @@ function Conge() {
           datefin: formattedEndDate,
         })
         .then(async (response) => {
-
           toast.success(response.data.message, {
             position: "top-center",
             autoClose: 3000,
@@ -42,12 +46,11 @@ function Conge() {
             draggable: true,
             progress: undefined,
           });
-
-
+          getConge();
+          Reset();
         })
         .catch(function (error) {
-            console.log(error.response.data.message
-                )
+          console.log(error.response.data.message);
           toast.error(error.response.data.message, {
             position: "top-center",
             autoClose: 3000,
@@ -70,118 +73,246 @@ function Conge() {
         progress: undefined,
       });
     }
-  }
-  const Reset = () => {
-    setNom('')
-    setDateRange('')
-    setJour('')
-    setDescription('')
-  }
-  const handlePrint = () => {
-    const tbodyContent = document.querySelector(
-      ".printts"
-    ).innerHTML;
-    const printWindow = window.open("", "_blank");
-
-    if (printWindow && tbodyContent) {
-      printWindow.document.write(`
-        <html>
-        <head>
-        <title>Information </title>
-      
-      </head>
-          <body>
-          <style>
-          .input-box label {
-            position: absolute;
-            top: 50%;
-            left: 5px;
-            transform: translateY(-50%);
-            font-size: 1em;
-            color: #fff;
-            pointer-events: none;
-            transition: 0.5s;
-        }</style>
-            <div className="table-hover">
-                <b>${tbodyContent}</b>
-            
-            </div>
-          </body>
-        </html>
-      `);
-      printWindow.document.close();
-      printWindow.print();
-    }
   };
+
+  const getConge = async () => {
+    await axios
+      .get(process.env.REACT_APP_API_URL + "/conges/get")
+      .then((response) => {
+        toast.success(response.data.message, {
+          position: "top-center",
+          autoClose: 3000,
+          hideProgressBar: false,
+          closeOnClick: false,
+          pauseOnHover: false,
+          draggable: false,
+          progress: undefined,
+        });
+
+        setAllConge(response.data.data);
+      })
+      .catch((error) => {
+        toast.error(
+          error.response.data.message
+            ? error.response.data.message
+            : error.message,
+          {
+            autoClose: 3000,
+            hideProgressBar: false,
+            closeOnClick: false,
+            pauseOnHover: false,
+            draggable: false,
+            progress: undefined,
+          }
+        );
+      });
+  };
+  const deleteSite = async () => {
+
+    await axios
+      .delete(process.env.REACT_APP_API_URL + "/conges/delete/" + conge._id)
+      .then((response) => {
+        setShow(false);
+        getConge();
+        toast.success(response.data.message, {
+          position: "top-center",
+          autoClose: 3000,
+          hideProgressBar: false,
+          closeOnClick: false,
+          pauseOnHover: false,
+          draggable: false,
+          progress: undefined,
+        });
+      })
+      .catch((error) => {
+        // console.log(error);
+        toast({
+          render: error.response.data.message
+            ? error.response.data.message
+            : error.message,
+          type: "error",
+          isLoading: false,
+          autoClose: 3000,
+          hideProgressBar: false,
+          closeOnClick: false,
+          pauseOnHover: false,
+          draggable: false,
+          progress: undefined,
+        });
+      });
+  };
+  const supModal = (item) => {
+    console.log(conge.name);
+    setShow(true);
+    setConge(item);
+  };
+  const Reset = () => {
+    setNom("");
+    setDateRange("");
+    setJour("");
+    setDescription("");
+  };
+
+
+  useEffect(() => {
+    getConge();
+  }, []);
   return (
-
-        <section className='homes'  >
-        <Topbar/>
-        <div className="headersform">
-        <h1>Demande  du congé</h1> 
+    <section className="homes">
+      <Topbar />
+      <div className="struc">
+        <div id="conges">
+          <h1>Demande du congé</h1>
+          <form action="">
+            <div className="printts">
+              <div className="table-hover">
+                <div className="input-box">
+                  <span className="icon">
+                    <i className="fa-solid fa-user"></i>
+                  </span>
+                  <input
+                    type="text"
+                    required
+                    value={nom}
+                    onChange={(e) => setNom(e.target.value)}
+                  />
+                  <label>Nom & Prénom</label>
+                </div>
+                <div className="input-box">
+                  <span className="icon">
+                    <i className="fas fa-hourglass-start"></i>
+                  </span>
+                  <input
+                    type="text"
+                    required
+                    value={jour}
+                    onChange={(e) => setJour(e.target.value)}
+                  />
+                  <label>Nombre du jour Congé</label>
+                </div>
+                <DatePicker
+                  selectsRange={true}
+                  dateFormat="yyyy/MM/dd"
+                  startDate={startDate}
+                  endDate={endDate}
+                  locale={fr}
+                  placeholderText="Date du  congé"
+                  onChange={(update) => {
+                    setDateRange(update);
+                  }}
+                  withPortal
+                />
+                <div className="input-box">
+                  <span className="icon">
+                    <i class="fa-brands fa-readme"></i>
+                  </span>
+                  <input
+                    type="textarea"
+                    required
+                    value={description}
+                    onChange={(e) => setDescription(e.target.value)}
+                  />
+                  <label>Raison du congé </label>
+                </div>
+              </div>{" "}
+            </div>
+            <div className="d-flex ">
+              <button type="reset" className="login me-2" onClick={Reset}>
+                {" "}
+                Annuler
+              </button>
+              <button type="button" className="login" onClick={createUser}>
+                Enregister
+              </button>
+            </div>
+          </form>
+          <Toaster />
+        </div>
         <div className="feuille">
-        <i class="fa-solid fa-print me-2"></i> 
-        <span  onClick={handlePrint}>
-          Imprimer
-        </span>
+          <h2>
+            Listes des congés<sup>({AllConge.length})</sup>
+          </h2>
+       
+          <div className="tous">
+         {AllConge.length==0? <span>Pas de  congé</span>
+          : <>{AllConge.map((cong) => {
+            return (
+              <div className="liste">
+                
+                <div className="titrecong">
+                  <span className="me-2">Nombres du jour {cong.jour}</span>
+                </div>
+                <div className="actions">
+                  <i class="fa-solid fa-print me-2 " ></i>
+                  <i
+                    class="fa-solid fa-trash"
+                    onClick={() => supModal(cong)}
+                  ></i>
+                </div>
+              </div>
+            );
+          })}
+         </>}
+          </div> 
         </div>
-        </div>
-    
+        <Modal
+          show={show}
+          onHide={handleClose}
+          backdrop="static"
+          keyboard={false}
+          className="modalDelete"
+          size="md"
+          id="addPageModal"
+          aria-labelledby="contained-modal-title-vcenter"
+          centered
+        >
+          <Modal.Header className="add_page_modal_header">
+            <div className="titre_popup_page">
+              <div className="modal_header_add_page ">
+                <div className="titre_popup_add_page">
+                  {" "}
+                  Supprimer utilisateur
+                </div>
 
-        <div id="conges" >
-        <form action="">
-        <div className="printts">
-          <div className="table-hover">
-          <div className="input-box">
-            <span className="icon">
-            <i className="fa-solid fa-user"></i>
-            </span>
-            <input type="text" required value={nom} onChange={(e) => setNom(e.target.value)}/>
-            <label>Nom & Prénom</label>
-          </div>
-         <div className="input-box">
-            <span className="icon">
-            <i className="fas fa-hourglass-start"></i>
-            </span>
-            <input type="text" required value={jour} onChange={(e) => setJour(e.target.value)}/>
-            <label>Nombre du jour Congé</label>
-          </div>
-          <DatePicker
-            selectsRange={true}
-            dateFormat="yyyy/MM/dd"
-            startDate={startDate}
-            endDate={endDate}
-            locale={fr}
-            placeholderText="Date du  congé"
-            onChange={(update) => {
-              
-              setDateRange(update);
-            }}
-          withPortal
-           />
-          <div className="input-box">
-            <span className="icon">
-            <i class="fa-brands fa-readme"></i>
-            </span>
-            <input type="textarea" required value={description} onChange={(e) => setDescription(e.target.value)}/>
-            <label>Raison du congé </label>
-          </div>
-          </div>  </div>
-          <div className="d-flex ">
-          <button type="reset" className="login me-2" onClick={Reset}>
-            {" "}
-            Annuler
-          </button>
-          <button type="button" className="login" onClick={createUser}>
-        
-            Enregister
-          </button>
-          </div>
-        </form>
-        <Toaster />
-    </div>
-      </section>
-  )
+                <i
+                  className="fa-solid fa-xmark closeSideBar"
+                  onClick={handleClose}
+                ></i>
+              </div>
+            </div>
+          </Modal.Header>
+          <Modal.Body>
+            <div className="add_page_modal_content ">
+              <div className="addPageOptions">
+                <div className="add_Page_Options_title">
+                  Voulez-vous vraiment supprimer l'utilisateur
+                  <b> {conge.name} ? </b>
+                  Cette action supprimera l'utilisateur ainsi que l'ensemble du
+                  contenu qui lui est associé.
+                  <br />
+                  Cette action ne peut pas être annulée.
+                </div>
+              </div>
+            </div>
+          </Modal.Body>
+          <Modal.Footer>
+            <button
+              className="btn btn-rounded  btn-outline-secondary"
+              onClick={handleClose}
+            >
+              Annuler
+            </button>
+            <button
+              className="btn  btn-rounded btn btn-outline-secondary delete"
+              onClick={deleteSite}
+            >
+              Supprimer
+            </button>
+          </Modal.Footer>
+        </Modal>
+      </div>
+    </section>
+  );
 }
 
-export default Conge
+export default Conge;
---
+++ src/components/Conge/StructConge/StructConge.jsx
@@ -0,0 +1,40 @@
+import React from 'react'
+
+function StructConge() {
+  return (
+    <div className='header'>
+      <table class="table">
+  <thead>
+    <tr>
+      <th scope="col">logo</th>
+      <th scope="col">Demande  de  congé</th>
+    </tr>
+  </thead>
+  <tbody>
+    <tr>
+      <th scope="row">Date de la  demande </th>
+      <td>Mark</td>
+    </tr>
+    <tr>
+      <th scope="row">Nom & prénom </th>
+      <td>Mark</td>
+    </tr>
+    <tr>
+      <th scope="row">Date debut congé</th>
+      <td>Mark</td>
+    </tr>
+    <tr>
+      <th scope="row">Date réprise</th>
+      <td>Mark</td>
+    </tr>
+    <tr>
+      <th scope="row">Nombre de  jours de  congés pris</th>
+      <td>Mark</td>
+    </tr>
+  </tbody>
+</table>
+    </div>
+  )
+}
+
+export default StructConge
