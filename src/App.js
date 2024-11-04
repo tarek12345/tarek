8f2ed76bfa99d37032f99c3669e5f1c18d9728b4
tarek12345 <tarekbenarfa53@gmail.com>
2024-11-04 11:06:16 +0100

    last  commit

--- src/App.js
+++ src/App.js
@@ -1,14 +1,10 @@
 import './App.css';
-import Header from './layout/header/header';
-import Footer from './layout/footer/footer';
 import Home from './pages/home/home';
 
 function App() {
   return (
     <div className="App">
-    <Header/>
     <Home/>
-    <Footer/>
     </div>
   );
 }
--- src/pages/home/home.jsx
+++ src/pages/home/home.jsx
@@ -1,11 +1,40 @@
-import React from 'react'
+import React, { useState } from 'react';
+import programming from "../../assets/programming.jpg";
+import Header from '../../layout/header/header';
+import Footer from '../../layout/footer/footer';
+export default function Home(props) {
+  const [language, setLanguage] = useState("FR");
 
-export default function Home() {
   return (
-    <div className='allsection'>
-      <div id="section1">
+    <>
+    <Header long={language}/>
+   
+    <div className="allsection ">
+      <div id="section1" className='container'>
+    
+        <div className='img-right'>
+    
+          <img src={programming} alt="sectionimageone" className='circular-image' />
+          <h2>Sur moi</h2>
+        </div>
+        <div className='img-left'>
         
+          <p>
+            Dynamique et polyvalent, je suis passionné par l'innovation et la
+            résolutionde problèmes. Fort d'une solide formation en ingénierie,
+            j'ai acquis une expertise dans le développement de solutions
+            technologiques efficaces et durables. Mon expérience professionnelle
+            m'a permis de développer des compétences en gestion de projet et en
+            travail d'équipe, ainsi qu'une capacité à m'adapter rapidement à de
+            nouveaux environnements. Toujoursavide d'apprendre et de relever de
+            nouveaux défis, je suis à la recherche d'opportunités où je pourrai
+            mettre à profit mes compétences et contribuer au succès de projets
+            stimulants
+          </p>
+        </div>
       </div>
     </div>
-  )
+     <Footer long={language}/>
+     </>
+  );
 }
