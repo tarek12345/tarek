2a7895fc1c8f80d00883dd4985dedc5afe7b4965
tarek12345 <tarekbenarfa53@gmail.com>
2024-11-18 14:37:39 +0100

    last version

--- src/componsants/ContactForm.jsx
+++ src/componsants/ContactForm.jsx
@@ -2,9 +2,12 @@ import React, { useRef } from 'react';
 import emailjs from '@emailjs/browser';
 import { ToastContainer, toast } from 'react-toastify';
 import 'react-toastify/dist/ReactToastify.css';
+import ReCAPTCHA from 'react-google-recaptcha';
 
 export default function ContactForm(tras) {
   const form = useRef();
+  const recaptchaRef = useRef(null);
+
   const serviceID = 'service_dx1ob8i';
   const templateID = 'template_ddbjogq';
   const userID = 'YZf-kCVH11ePwsLgO';
@@ -13,6 +16,18 @@ export default function ContactForm(tras) {
   const sendEmail = (e) => {
     e.preventDefault();
 
+    // Récupérer la réponse reCAPTCHA
+    const recaptchaValue = recaptchaRef.current.getValue();
+    if (!recaptchaValue) {
+      // Si le reCAPTCHA n'est pas validé
+      toast.error('Veuillez valider le reCAPTCHA avant de soumettre le formulaire.', {
+        position: 'bottom-right',
+        autoClose: 3000,
+      });
+      return;
+    }
+
+    // Continuer avec l'envoi si reCAPTCHA validé
     emailjs
       .sendForm(serviceID, templateID, form.current, {
         publicKey: userID,
@@ -21,16 +36,17 @@ export default function ContactForm(tras) {
         () => {
           // Affichage d'un toast de succès
           toast.success('Message envoyé avec succès!', {
-            position: 'bottom-right',  // Spécifiez directement la position en chaîne de caractères
-            autoClose: 3000, // Ferme après 3 secondes
+            position: 'bottom-right',
+            autoClose: 3000,
           });
           form.current.reset(); // Réinitialiser le formulaire après envoi
+          recaptchaRef.current.reset(); // Réinitialiser le reCAPTCHA
         },
         (error) => {
           // Affichage d'un toast d'erreur
-          toast.error('Erreur lors de l\'envoi du message. Veuillez réessayer.', {
-            position: 'bottom-right',  // Spécifiez directement la position en chaîne de caractères
-            autoClose: 3000, // Ferme après 3 secondes
+          toast.error("Erreur lors de l'envoi du message. Veuillez réessayer.", {
+            position: 'bottom-right',
+            autoClose: 3000,
           });
           console.log('FAILED...', error.text);
         }
@@ -47,7 +63,7 @@ export default function ContactForm(tras) {
             name="user_name"
             id="user_name"
             className="form-control"
-             placeholder={tras.long==="FR"?"Nom":"Name"}
+            placeholder={tras.long === 'FR' ? 'Nom' : 'Name'}
             required
           />
         </div>
@@ -57,7 +73,7 @@ export default function ContactForm(tras) {
             name="user_email"
             id="user_email"
             className="form-control"
-            placeholder={tras.long==="FR"?'Email':'Email'}
+            placeholder={tras.long === 'FR' ? 'Email' : 'Email'}
             required
           />
         </div>
@@ -67,11 +83,18 @@ export default function ContactForm(tras) {
             id="message"
             className="form-control"
             rows="4"
-             placeholder={tras.long==="FR"?'Message':'Message'}
+            placeholder={tras.long === 'FR' ? 'Message' : 'Message'}
             required
           ></textarea>
         </div>
-        <button type="submit" className="btn-hover color-7">{tras.long==="FR"?"Envoyer":"Send"}</button>
+        <ReCAPTCHA
+          ref={recaptchaRef}
+          sitekey="6Lc3oYIqAAAAAFTxs6emwU2Z8onowyIEZ65qx1fM"
+        />
+
+        <button type="submit" className="btn-hover color-7">
+          {tras.long === 'FR' ? 'Envoyer' : 'Send'}
+        </button>
       </form>
 
       {/* Composant ToastContainer pour afficher les notifications */}
--- src/pages/home/sectionhome/competences.jsx
+++ src/pages/home/sectionhome/competences.jsx
@@ -1,4 +1,11 @@
-import React, { useState } from 'react';
+import React, { useState, useEffect } from 'react';
+import { Swiper, SwiperSlide } from 'swiper/react';
+// Import Swiper styles
+import 'swiper/css';
+import 'swiper/css/free-mode';
+import 'swiper/css/pagination';
+// Import required modules
+import { Pagination, Navigation } from 'swiper/modules';
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
 import { faArrowDown,faSearch,faCode,faLayerGroup    } from '@fortawesome/free-solid-svg-icons'; // Icônes solid
 import {
@@ -11,7 +18,6 @@ import {
   faWordpress 
 } from '@fortawesome/free-brands-svg-icons'; // Icônes brands
 import { faVuejs } from '@fortawesome/free-brands-svg-icons'; // Import de l'icône Vue.js
-
 export default function Competences(props) {
   // État local pour stocker les niveaux de compétence
   const [skills] = useState([
@@ -31,7 +37,20 @@ export default function Competences(props) {
     { name: "Wordpress Gutenberg", level: 90, icon: faWordpress }, // Si faNode n'est pas disponible, utilisez une autre icône appropriée.
     { name: "Seo", level: 80, icon: faSearch }, // Si faNode n'est pas disponible, utilisez une autre icône appropriée
   ]);
-
+  const [isMobile, setIsMobile] = useState(false);
+    // Hook to detect mobile screen
+    useEffect(() => {
+      const handleResize = () => {
+        setIsMobile(window.innerWidth < 768); // Define mobile breakpoint (e.g., 768px)
+      };
+  
+      handleResize(); // Initial check
+      window.addEventListener('resize', handleResize);
+  
+      return () => {
+        window.removeEventListener('resize', handleResize);
+      };
+    }, []);
   return (
     <div id="section2">
       <div className="img-right">
@@ -40,7 +59,7 @@ export default function Competences(props) {
           <FontAwesomeIcon icon={faArrowDown} />
         </h2>
       </div>
-      <div className="skills">
+      {!isMobile? <div className="skills">
         {skills.map((skill, index) => (
           <div className="card-skills" key={index}>
             <FontAwesomeIcon icon={skill.icon} />{" "}
@@ -59,6 +78,38 @@ export default function Competences(props) {
           </div>
         ))}
       </div>
+      :
+      <Swiper
+      modules={[Pagination, Navigation]}
+      spaceBetween={20}
+      slidesPerView={1}
+      navigation
+      pagination={{ clickable: true }}
+      breakpoints={{
+        640: { slidesPerView: 2 },
+        768: { slidesPerView: 3 },
+      }}
+    >
+      {skills.map((skill, index) => (
+        <SwiperSlide key={index}>
+          <div className="card-skills">
+            <FontAwesomeIcon icon={skill.icon} size="2x" />
+            <span>{skill.name}</span>
+            <div className="circle-container">
+              {[...Array(10)].map((_, circleIndex) => (
+                <div
+                  key={circleIndex}
+                  className={`circle ${
+                    circleIndex < Math.ceil(skill.level / 10) ? "filled" : ""
+                  }`}
+                />
+              ))}
+            </div>
+          </div>
+        </SwiperSlide>
+      ))}
+    </Swiper>}
+     
     </div>
   );
 }
