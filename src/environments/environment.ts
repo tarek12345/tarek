e5a5479ddd923577290f30066341c3c7887cc752
tarek12345 <tarekbenarfa53@gmail.com>
2025-11-25 21:26:37 +0100

    add last  version project

--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -1,6 +1,10 @@
-export const environment = {
-    production: false,
-    apiUrl: 'http://127.0.0.1:8000/api',
-    baseUrl: 'http://127.0.0.1:8000'
-  };
-  
+// export const environment = {
+//     production: false,
+//     apiUrl: 'http://127.0.0.1:8000/api',
+//     baseUrl: 'http://127.0.0.1:8000'
+//   };
+  export const environment = {
+  production: true,
+  apiUrl: 'https://ton-backend.onrender.com/api/',
+  baseUrl: 'https://ton-backend.onrender.com'
+};
