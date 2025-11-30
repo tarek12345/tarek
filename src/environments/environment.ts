b14e3ef0a240963b6e877c8bd22b9e2becddedff
tarek12345 <tarekbenarfa53@gmail.com>
2025-11-30 19:09:56 +0100

    last migration

--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -5,6 +5,6 @@
 //   };
 export const environment = {
   production: true,
-  apiUrl: "https://tarek-x9f7.onrender.com/api/",
+  apiUrl: "https://tarek-x9f7.onrender.com/api",
   baseUrl: "https://tarek-x9f7.onrender.com"
 };
